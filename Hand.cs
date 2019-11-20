8c9730b0acefb90ec30949f3a974bbcd1111e565
Matthew Patterson <matthewp414@gmail.com>
2019-11-20 13:41:02 -0600

    Just made my strating classes and trying to figure out what I do with my list of moves

---
+++ Hand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+
+    class Hand 
+    {
+        List<string> moves;
+        int score = 0;
+
+        public Hand()
+        {
+            moves.Add("Rock");
+            moves.Add("Paper");
+            moves.Add("Scissors");
+            moves.Add("Lizard");
+            moves.Add("Spock");
+            
+
+        }
+
+
+
+
+    }
+}
